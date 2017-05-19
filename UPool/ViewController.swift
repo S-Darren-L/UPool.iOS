337cfce8a7e6ad19819d222a99a19a79b8539145
S-Darren-Liu <gulang15@gmail.com>
2017-05-19 02:49:37 -0400

    create project

---
+++ UPool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UPool
+//
+//  Created by Darren on 2017-05-19.
+//  Copyright © 2017 UPool.iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

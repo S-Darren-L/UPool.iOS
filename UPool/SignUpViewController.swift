1f93d4aa4779c2a22a66acf1262f8c5eaf7d47ae
S-Darren-Liu <gulang15@gmail.com>
2017-05-22 14:50:24 -0400

    Add navigation bar on sign up view

--- UPool/SignUpViewController.swift
+++ UPool/SignUpViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
+    @IBOutlet var naviItem: UINavigationItem!
+    @IBOutlet var naviBackButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
+        
+        //Set navigation bar title
+        naviItem.title = "SIGN_UP".localized
+        //Set navigation back button
+        naviBackButton = UIBarButtonItem(title: "BACK".localized, style: .plain, target: self, action: #selector(navigateBack))
+        naviBackButton.tintColor = UIColor.black
+        naviItem.leftItemsSupplementBackButton = true
+        naviItem.leftBarButtonItem = naviBackButton
+    }
+    
+    func navigateBack(sender:UIButton) {
+        performNavigationBack()
     }
 }
--- UPool/UIViewControllerExtension.swift
+++ UPool/UIViewControllerExtension.swift
@@ -10,6 +10,8 @@ import Foundation
 import UIKit
 
 extension UIViewController {
+    
+    //Dismiss keyboard when tap around
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
         tap.cancelsTouchesInView = false
@@ -18,5 +20,14 @@ extension UIViewController {
     
     func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    //Navigate back to previous view
+    func performNavigationBack()  {
+        if let nav = self.navigationController {
+            nav.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }
